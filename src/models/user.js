b9d6650bf36bcd8a9aa6b15acc5553db7174d25b
unknown <dipeshmaywade@gmail.com>
2021-05-30 11:41:53 +0530

    feat: create schema for user registration

---
+++ src/models/user.js
@@ -0,0 +1,41 @@
+/**
+ * @module        models
+ * @file          user.js
+ * @description   mongoose schema for users
+ * @requires      {@link http://mongoosejs.com/|mongoose}
+ * @author        Dipesh Maywade <dipeshmaywade@gmail.com>
+----------------------------------------------------------------------------------------------------*/
+
+const mongoose = require('mongoose');
+
+const user = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      require: true,
+      match: [/^[A-Z]{1}[a-z]{2,}$/, 'please enter valid Name'],
+    },
+    lastName: {
+      type: String,
+      require: true,
+      match: [/^[A-Z]{1}[a-z]{2,}$/, 'please enter valid Name'],
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+      match: [/^[a-z0-9](\.?[a-z0-9]){5,}@g(oogle)?mail\.com$/, 'please enter valid email'],
+    },
+    password: {
+      type: String,
+    },
+    role: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const userRegistration = mongoose.model('User', user);
+
+module.exports = { userRegistration };
