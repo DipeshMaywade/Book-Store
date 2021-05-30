be5929dc5db12e5c35006bc37d9bfedb56efd26d
unknown <dipeshmaywade@gmail.com>
2021-05-30 13:02:08 +0530

    feat: add user mutation add and export through index.js to server.js

---
+++ src/queryAndMutaion/index.js
@@ -0,0 +1,26 @@
+/**
+ * @module        queryAndMutation
+ * @file          index.js
+ * @description   this is the main index file which contain all the query and mutaions related to fundooApp
+ *                and create and grapghQL schema which is used bt graphQLHTTP middleware.
+ * @requires      graphql{@linkhttps://www.npmjs.com/package/graphql}
+ * @author        Dipesh Maywade <dipeshmaywade@gmail.com>
+----------------------------------------------------------------------------------------------------*/
+
+const { GraphQLSchema, GraphQLObjectType } = require('graphql');
+const { addUser } = require('./user/user.mutation');
+
+const query = new GraphQLObjectType({
+  name: 'Query',
+  fields: {},
+});
+
+const mutation = new GraphQLObjectType({
+  name: 'Mutations',
+  fields: { addUser },
+});
+
+module.exports = new GraphQLSchema({
+  query,
+  mutation,
+});
--- src/queryAndMutaion/user/user.mutation.js
+++ src/queryAndMutaion/user/user.mutation.js
@@ -6,3 +6,58 @@
  * @author        Dipesh Maywade <dipeshmaywade@gmail.com>
 ----------------------------------------------------------------------------------------------------*/
 const { GraphQLNonNull, GraphQLString } = require('graphql');
+const { userRegistration } = require('../../models/user');
+const { userType, response } = require('../../type/user');
+
+class Mutation {
+  /**
+   * @fileds addUser
+   * @type userType
+   * @argument args
+   * @param {resolveParameter} root
+   * @param {resolveParameter} data
+   * @description addUser fields provide ability to create new user account for
+   */
+  addUser = {
+    type: response,
+    args: {
+      firstName: {
+        type: new GraphQLNonNull(GraphQLString),
+      },
+      lastName: {
+        type: new GraphQLNonNull(GraphQLString),
+      },
+      email: {
+        type: new GraphQLNonNull(GraphQLString),
+      },
+      password: {
+        type: new GraphQLNonNull(GraphQLString),
+      },
+      role: {
+        type: new GraphQLNonNull(GraphQLString),
+      },
+    },
+    resolve: async (root, data) => {
+      try {
+        const userModel = new userRegistration(data);
+        const newUser = await userModel.save();
+        if (!newUser) {
+          return {
+            success: false,
+            message: 'failed to save',
+          };
+        }
+        return {
+          success: true,
+          message: 'new user added successfully...!!',
+        };
+      } catch (error) {
+        loggers.error(`error`, error);
+        return {
+          success: false,
+          message: 'failed to save',
+        };
+      }
+    },
+  };
+}
